522023a5c4dc2c139988b53bbdbc7165740f6e19
pbb <1305355506@qq.com>
2019-10-23 22:02:29 +0800

    first

---
+++ jdbc/jdbc_01/src/jdbc_01/JdbcDemo1.java
@@ -0,0 +1,35 @@
+package jdbc_01;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class JdbcDemo1 {
+    public static void main(String[] args) throws Exception {
+        // 1.导入jar包
+        // 2.注册驱动
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        // 3.获取数据库连接对象
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/yiibaidb", "root", "MySQL0504");
+        // 4.定义sql语句
+        String sql = "select lastName,firstName " +
+                "from employees " +
+                "where officeCode = 1";
+        //5. 获取执行sql的对象
+        Statement statement = connection.createStatement();
+        // 6.执行sql
+        ResultSet s = statement.executeQuery(sql);
+        // 7.处理结果
+        String lastName, firstName;
+        System.out.println("lastName\tfirstName\r");
+        while (s.next()) {
+            lastName = s.getString("lastName");
+            firstName = s.getString("firstName");
+            System.out.println(lastName + "\t" + firstName + "\t");
+        }
+        // 8.关闭资源
+        statement.close();
+        connection.close();
+    }
+}
