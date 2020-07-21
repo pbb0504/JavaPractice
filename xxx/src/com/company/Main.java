3be882ff11dd871cc44d826dbcdbb0fcdd7bf7a5
panbinbin <1305355506@qq.com>
2020-07-21 17:35:23 +0800

    scroll

--- xxx/src/com/company/Main.java
+++ xxx/src/com/company/Main.java
@@ -12,6 +12,7 @@ public class Main {
         int windowSize = 5;
         char[] chars = s.toCharArray();
         int currentIndex = 0;
+        // 显示12345,23456,34567,45678
         while ((currentIndex + windowSize - 1) < chars.length) {
             for (int i = currentIndex; i < currentIndex + windowSize; i++) {
                 System.out.print(chars[i]);
