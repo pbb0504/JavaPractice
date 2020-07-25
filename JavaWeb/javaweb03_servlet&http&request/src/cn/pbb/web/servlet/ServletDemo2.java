8360b2d00da4d2fdaea34f903b3fcfea1a560faf
panbinbin <1305355506@qq.com>
2020-07-25 16:21:12 +0800

    .

---
+++ JavaWeb/javaweb03_servlet&http&request/src/cn/pbb/web/servlet/ServletDemo2.java
@@ -0,0 +1,20 @@
+package cn.pbb.web.servlet;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+/**
+ * @author: pbb
+ * @date: 2020/7/25 15:47
+ */
+@WebServlet("/demo2")
+public class ServletDemo2 extends GenericServlet {
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("genericServlet");
+    }
+}
