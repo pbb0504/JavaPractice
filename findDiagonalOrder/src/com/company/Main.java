549f61303fe14e1aff8c3975e82449207154fcf5
panbinbin <1305355506@qq.com>
2020-08-04 13:49:19 +0800

    findDiagonalOrder

---
+++ findDiagonalOrder/src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+/**
+ * @author 13053
+ */
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    public int[] findDiagonalOrder(int[][] matrix) {
+        int[] res = new int[matrix.length* matrix[0].length];
+        /*
+            1,2,3,10
+            4,5,6,11
+            7,8,9,12
+
+
+         */
+
+        return null;
+    }
+}
