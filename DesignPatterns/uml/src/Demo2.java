c39e86d71277ef17cd6b59a9d95d4d98ac5cc053
panbinbin <1305355506@qq.com>
2020-08-18 20:52:06 +0800

    uml

---
+++ DesignPatterns/uml/src/Demo2.java
@@ -0,0 +1,16 @@
+/**
+ * @author: pbb
+ * @date: 2020/8/18 10:53
+ */
+public class Demo2 {
+    private Integer id;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
