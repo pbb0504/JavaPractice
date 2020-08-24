0b6462fac1eabfad711f6c5ce315e289d3c658de
panbinbin <1305355506@qq.com>
2020-08-24 23:06:43 +0800

    observer

---
+++ DesignPatterns/observer/src/WeatherData.java
@@ -0,0 +1,21 @@
+/**
+ * @author: pbb
+ * @date: 2020/8/24 22:55
+ */
+public class WeatherData {
+    void getTemperature() {
+
+    }
+
+    void getHumidity() {
+
+    }
+
+    void getPressure() {
+
+    }
+
+    void measurementChanged() {
+        
+    }
+}
