782029ed3655ba62fba130675e530860469898fc
panbinbin <1305355506@qq.com>
2020-09-06 15:13:38 +0800

    factory pattern next but not the last

--- DesignPatterns/FactoryPattern2/src/Main.java
+++ DesignPatterns/FactoryPattern2/src/Main.java
@@ -1,10 +1,12 @@
+import PizzaStore.PizzaStore1;
+
 /**
  * @author: pbb
  * @date: 2020/9/5 22:47
  */
 public class Main {
     public static void main(String[] args) {
-
-
+        PizzaStore1 pizzaStore1 = new PizzaStore1();
+        pizzaStore1.orderPizza("cheese");
     }
 }
